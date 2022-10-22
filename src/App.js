8832097d2a3c5d8e84353f7168187b4b7e08e5df
Hirva Dhandhukia <hirva.dhandhukia@gmail.com>
2022-10-23 01:22:49 +0530

    new product components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Footer from './components/Footer';
 
 function App() {
+  const productList = [
+    {
+      price: 99999,
+      name: "The Psychology of Money",
+      quantity: 0
+    },
+    {
+      price: 9999,
+      name: "War of Lanka",
+      quantity: 0
+    }
+  ]
+
   return (
-    <>
+    <React.Fragment>
       <Navbar />
-    </>
+      <main className='container mt-5'>
+        <ProductList productList={productList} />
+      </main>
+      {/* <Footer /> */}
+    </React.Fragment>
   );
 }
 
---
+++ src/components/ProductList.js
@@ -0,0 +1,12 @@
+// this is a functional component
+import React from 'react';
+import Product from './Product';
+
+export default function ProductList(props) {
+    console.log(props);
+  return (
+    props.productList.map((product,i)=>{
+        return <Product product={product} key={i} />
+    })
+  )
+}
